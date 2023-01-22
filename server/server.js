ee54e5a1635516ca447ffcc73a733fd7fffb86ee
Jay Dimock <jdpickle@gmail.com>
2023-01-21 20:30:19 -0800

    installed helmet for security

--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ require("dotenv").config();
 require("./config/mongoose.config");
 const PORT = process.env.PORT || 8000;
 const app = express();
+const helmet = require("helmet");
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet()); // help protect your app from well-known web vulnerabilities
 
 const PlaylistsRoutes = require("./routes/playlists.routes");
 PlaylistsRoutes(app);
